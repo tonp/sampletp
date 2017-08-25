a2d51e61d9502782888a93b226e2a9eb98b697c8
tonp <tonp@oregonstate.edu>
2017-08-24 17:10:03 -0700

    kjk

--- js/script.js
+++ js/script.js
@@ -1,26 +1,21 @@
+/* 
+*	Load contents with fade animation.
+*
+*/
 $(document).ready(function(){
 	
-	// When on the page
 	$('.content.al').load('about.html');
 	$('a:first-child').css("color", "#E47A3C");
 	
 	$('.menu.al a').click(function(event){
 		
-		var toLoad = $(this).attr('href'); // the attribute to the a element 
-		
+		var toLoad = $(this).attr('href'); 
 		$('.menu.al a').css('color', '#8D928E');
 		$(this).css("color", "#E47A3C");
 	
-		 //$('.content.al').load(toLoad+'.html');
-		
-		
 		$(".content.al").fadeOut(1).load(toLoad+'.html').fadeIn(500);
-		
-		
-
-		
+	
 		return false;
 	});
-	
 	
 });
